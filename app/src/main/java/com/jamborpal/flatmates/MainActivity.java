fddc13f57412188fa66b638fb82ceb0b3aa1e9bb
jamborpal <jamborpal0@gmail.com>
2021-03-24 11:37:37 +0100

    Start adding drawer view

--- app/src/main/java/com/jamborpal/flatmates/MainActivity.java
+++ app/src/main/java/com/jamborpal/flatmates/MainActivity.java
@@ -1,14 +1,74 @@
 package com.jamborpal.flatmates;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    NavController navController;
+    AppBarConfiguration appBarConfiguration;
+    DrawerLayout drawerLayout;
+    NavigationView navigationDrawer;
+    BottomNavigationView bottomNavigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+        setupNavigation();
+    }
+
+    private void initViews() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationDrawer = findViewById(R.id.navigation_drawer);
+
+        toolbar = findViewById(R.id.toolbar);
+    }
+
+    private void setupNavigation() {
+       navController = Navigation.findNavController(this, R.id.host_fragment);
+        setSupportActionBar(toolbar);
+        NavigationUI.setupWithNavController(navigationDrawer, navController);
+
+    }
+
+
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START))
+            drawerLayout.closeDrawer(GravityCompat.START);
+        else
+            super.onBackPressed();
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //NavController navController = Navigation.findNavController(this, R.id.host_fragment);
+        return NavigationUI.onNavDestinationSelected(item, navController) || super.onOptionsItemSelected(item);
     }
 }
