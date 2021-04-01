8cb990d130bdbc1cf18b8cdac19f597873d964f2
jamborpal <jamborpal0@gmail.com>
2021-04-01 12:35:18 +0200

    changes1

--- app/src/main/java/com/jamborpal/flatmates/MainActivity.java
+++ app/src/main/java/com/jamborpal/flatmates/MainActivity.java
@@ -1,5 +1,11 @@
 package com.jamborpal.flatmates;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,11 +17,6 @@ import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
@@ -32,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e("Main","BITCH3");
         initViews();
         setupNavigation();
     }
@@ -39,17 +41,39 @@ public class MainActivity extends AppCompatActivity {
     private void initViews() {
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationDrawer = findViewById(R.id.navigation_drawer);
-
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view);
         toolbar = findViewById(R.id.toolbar);
     }
 
     private void setupNavigation() {
-       navController = Navigation.findNavController(this, R.id.host_fragment);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         setSupportActionBar(toolbar);
+        appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_pokedex,
+                R.id.navigation_teams)
+                .setOpenableLayout(drawerLayout)
+                .build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
         NavigationUI.setupWithNavController(navigationDrawer, navController);
-
+        setBottomNavigationVisibility();
     }
 
+    private void setBottomNavigationVisibility() {
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
+                switch (destination.getId()) {
+                    default:
+                        bottomNavigationView.setVisibility(View.GONE);
+                        break;
+                    case R.id.nav_pokedex:
+                    case R.id.navigation_teams:
+                        bottomNavigationView.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+    }
 
 
     @Override
@@ -65,10 +89,15 @@ public class MainActivity extends AppCompatActivity {
             super.onBackPressed();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.appbar, menu);
+        return true;
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        //NavController navController = Navigation.findNavController(this, R.id.host_fragment);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.onNavDestinationSelected(item, navController) || super.onOptionsItemSelected(item);
     }
 }
