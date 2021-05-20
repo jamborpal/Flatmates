a5043e423f42bf2dc2682c087bd369ed6b51d81e
jamborpal <jamborpal0@gmail.com>
2021-05-20 17:23:08 +0200

    DONE

--- app/src/main/java/com/jamborpal/app/MainActivity.java
+++ app/src/main/java/com/jamborpal/app/MainActivity.java
@@ -2,6 +2,7 @@ package com.jamborpal.app;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -28,12 +29,21 @@ public class MainActivity extends AppCompatActivity {
         // menu should be considered as top level destinations.
 
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_takemehome, R.id.nav_settings, R.id.nav_contact,R.id.nav_events,R.id.nav_expenses,R.id.nav_tasks,R.id.nav_messageboard)
+                R.id.nav_home, R.id.nav_takemehome, R.id.nav_settings, R.id.nav_contact, R.id.nav_events, R.id.nav_expenses, R.id.nav_tasks, R.id.nav_messageboard)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getItemId() == R.id.action_settings) {
+                    navController.navigate(R.id.nav_settings);
+                }
+                return false;
+            }
+        });
 
     }
 
